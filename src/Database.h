f13b625a781efcd6b299ca575e626206fca17c2f
IlyaChichkov <ilya555chich@yandex.ru>
2024-03-05 10:38:12 +0300

    feat: implementation database class members

--- src/Database.h
+++ src/Database.h
@@ -5,10 +5,26 @@
 #ifndef UNTITLED_DATABASE_H
 #define UNTITLED_DATABASE_H
 
+#include <iostream>
+#include <vector>
+#include "Employee.h"
+
 namespace ERS {
 
-    class Database {
+    const int c_FirstEmployeeNumber = 1'000;
 
+    class Database {
+    public:
+        Employee& addEmployee(const std::string& firstName, const std::string& lastName);
+        Employee& getEmployee(int employeeNumber);
+
+        void displayAll() const;
+        void displayCurrent() const;
+        void displayFormer() const;
+
+    private:
+        std::vector<Employee> m_employees;
+        int m_nextEmployeeNumber = c_FirstEmployeeNumber;
     };
 
 } // ERS
