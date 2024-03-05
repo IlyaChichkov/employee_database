7d4ff43e44e8539555aa2ae04e62ad2ccdf7ae91
IlyaChichkov <ilya555chich@yandex.ru>
2024-03-05 10:44:51 +0300

    feat: initialization database class methods

--- src/Database.cpp
+++ src/Database.cpp
@@ -5,4 +5,50 @@
 #include "Database.h"
 
 namespace ERS {
+    Employee &Database::addEmployee(const std::string &firstName, const std::string &lastName) {
+        Employee employee(firstName, lastName);
+        employee.setEmployeeNumber(m_nextEmployeeNumber++);
+        employee.hire();
+
+        m_employees.push_back(employee);
+        return m_employees.back();
+    }
+
+    Employee &Database::getEmployee(int employeeNumber) {
+        for(auto& employee : m_employees)
+        {
+            if(employee.getEmployeeNumber() == employeeNumber)
+            {
+                return employee;
+            }
+        }
+        throw std::logic_error { "No employee found." };
+    }
+
+    void Database::displayAll() const {
+        for(auto& employee : m_employees)
+        {
+            employee.display();
+        }
+    }
+
+    void Database::displayCurrent() const {
+        for(auto& employee : m_employees)
+        {
+            if(employee.isHired())
+            {
+                employee.display();
+            }
+        }
+    }
+
+    void Database::displayFormer() const {
+        for(auto& employee : m_employees)
+        {
+            if(!employee.isHired())
+            {
+                employee.display();
+            }
+        }
+    }
 } // ERS
