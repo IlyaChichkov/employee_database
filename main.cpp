9e811aa0c507a201061c34f5009e7bd579e18b59
IlyaChichkov <ilya555chich@yandex.ru>
2024-03-05 11:32:27 +0300

    feat: main method initialization

--- main.cpp
+++ main.cpp
@@ -1,36 +1,18 @@
 #include <iostream>
-#include <limits>
 #include <vector>
+#include <memory>
+#include "src/Employee.h"
+#include "src/Database.h"
+#include "src/UserInterface.h"
 
 using namespace std;
 
-enum class PieceType { King, Queen, Rook, Pawn };
-
-void Sort(std::vector<int> &arr)
-{
-    int i, j;
-    auto n = arr.size();
-
-    for (i = 0; i < n - 1; ++i) {
-        cout << "i: " << i << "\n";
-        for (j = 0; j < n - i - 1; ++j) {
-            cout << "j: " << j << " ";
-            if(arr.at(j) > arr.at(j + 1))
-            {
-                cout << "swap\n";
-                swap(arr.at(j), arr.at(j + 1));
-            }
-        }
-    }
-}
-
 int main() {
-    std::vector<int> array { 5, 6, 2, 7, 12, 85, 25, 7, 7, 3, 4, 4, 6, 65 , 1};
-    Sort(array);
-    for (size_t i = 0; i < array.size(); ++i) {
-        cout << array.at(i) << ", ";
-    }
-    cout << endl;
+    ERS::Database database;
+    ERS::UserInterface userInterface;
+    userInterface.setDatabase(std::make_shared<ERS::Database>(database));
+
+    userInterface.displayMainMenu();
 
     return 0;
 }
