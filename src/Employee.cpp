796c4a97a87800bf650e7255e247f942629d8f0f
IlyaChichkov <ilya555chich@yandex.ru>
2024-03-05 10:24:58 +0300

    feat: initialized employee class members

--- src/Employee.cpp
+++ src/Employee.cpp
@@ -6,4 +6,63 @@
 
 namespace ERS {
 
+    Employee::Employee(const std::string &firstName, const std::string &lastName) {
+
+    }
+
+    void Employee::promote(int raiseAmount) {
+
+    }
+
+    void Employee::demote(int demeritAmount) {
+
+    }
+
+    void Employee::hire() {
+
+    }
+
+    void Employee::fire() {
+
+    }
+
+    void Employee::display() const {
+
+    }
+
+    void Employee::setFirstName(const std::string &firstName) {
+
+    }
+
+    const std::string &Employee::getFirstName() const {
+        return "";
+    }
+
+    void Employee::setLastName(const std::string &lastName) {
+
+    }
+
+    const std::string &Employee::getLastName() const {
+        return "";
+    }
+
+    void Employee::setEmployeeNumber(int employeeNumber) {
+
+    }
+
+    int Employee::getEmployeeNumber() const {
+        return 0;
+    }
+
+    void Employee::setSalary(int newSalary) {
+
+    }
+
+    int Employee::getSalary() const {
+        return 0;
+    }
+
+    bool Employee::isHired() const {
+        return false;
+    }
 } // ERS
--- src/Employee.h
+++ src/Employee.h
@@ -9,9 +9,42 @@
 #include <string>
 
 namespace ERS {
-    class Employee {
 
+    const int c_DefaultStartingSalary = 30'000;
+    const int c_DefaultRaiseAndDemeritAmount = 1'000;
+
+    class Employee {
+    public:
+        Employee(const std::string& firstName,
+                 const std::string& lastName);
+        void promote(int raiseAmount = c_DefaultRaiseAndDemeritAmount);
+        void demote(int demeritAmount = c_DefaultRaiseAndDemeritAmount);
+        void hire();
+        void fire();
+        void display() const;
+
+        void setFirstName(const std::string& firstName);
+        const std::string& getFirstName() const;
+
+        void setLastName(const std::string& lastName);
+        const std::string& getLastName() const;
+
+        void setEmployeeNumber(int employeeNumber);
+        int getEmployeeNumber() const;
+
+        void setSalary(int newSalary);
+        int getSalary() const;
+
+        bool isHired() const;
+
+    private:
+        std::string m_firstName;
+        std::string m_lastName;
+        int m_employeeNumber = -1;
+        int m_salary = c_DefaultStartingSalary;
+        bool m_hired = false;
     };
+
 } // ERS
 
 #endif //UNTITLED_EMPLOYEE_H
