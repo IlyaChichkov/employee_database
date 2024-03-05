82d716c5552f5d450090dd9c61358bd80beb2c45
IlyaChichkov <ilya555chich@yandex.ru>
2024-03-05 10:31:38 +0300

    feat: implemented employee class methods

--- src/Employee.cpp
+++ src/Employee.cpp
@@ -7,62 +7,67 @@
 namespace ERS {
 
     Employee::Employee(const std::string &firstName, const std::string &lastName) {
-
+        m_firstName = firstName;
+        m_lastName = lastName;
     }
 
     void Employee::promote(int raiseAmount) {
-
+        setSalary(getSalary() + raiseAmount);
     }
 
     void Employee::demote(int demeritAmount) {
-
+        setSalary(getSalary() - demeritAmount);
     }
 
     void Employee::hire() {
-
+        m_hired = true;
     }
 
     void Employee::fire() {
-
+        m_hired = false;
     }
 
     void Employee::display() const {
-
+        std::cout << "> Employee:" << std::endl;
+        std::cout << "  First name: " << getFirstName() << "  last name: " << getLastName() << std::endl;
+        std::cout << "  Employee number: " << getEmployeeNumber() << std::endl;
+        std::cout << "  Salary: " << getSalary() << std::endl;
+        std::cout << "  Is hired: " << (isHired() ? "True" : "False") << std::endl;
     }
 
     void Employee::setFirstName(const std::string &firstName) {
-
+        m_firstName = firstName;
     }
 
     const std::string &Employee::getFirstName() const {
-        return "";
+        return m_firstName;
     }
 
     void Employee::setLastName(const std::string &lastName) {
-
+        m_lastName = lastName;
     }
 
     const std::string &Employee::getLastName() const {
-        return "";
+        return m_lastName;
     }
 
     void Employee::setEmployeeNumber(int employeeNumber) {
-
+        m_employeeNumber = employeeNumber;
     }
 
     int Employee::getEmployeeNumber() const {
-        return 0;
+        return m_employeeNumber;
     }
 
     void Employee::setSalary(int newSalary) {
-
+        m_salary = newSalary;
     }
 
     int Employee::getSalary() const {
-        return 0;
+        return m_salary;
     }
 
     bool Employee::isHired() const {
-        return false;
+        return m_hired;
     }
 } // ERS
